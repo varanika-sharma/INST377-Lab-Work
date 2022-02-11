941954592e8897e73d20d959b2df48ecf7627cd0
Vara <varanikasharma1@gmail.com>
2022-02-11 17:34:10 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
   const width = 10;
+
+  console.log(squares);
 })
