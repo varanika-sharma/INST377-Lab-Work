5b592cc940666900c135a2411eeafc1e79d9cdc4
Vara <varanikasharma1@gmail.com>
2022-02-11 22:23:16 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
-  const squares = Array.from(document.querySelectorAll('.grid div'));
+  let squares = Array.from(document.querySelectorAll('.grid div'));
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
@@ -10,30 +10,40 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width+1, width+2, width+2+2],
     [1, width +1, width +2+1, width+2],
     [width, width+2, width +2+1, width+2+2]
-  ]
+  ];
   const zTetromnino = [
     [0,width,widh+1,width+2+1],
     [width+1,width+2,width+2,width+2+1],
     [0,width,width+1,width+2+1],
     [width+1,width+2,width+2,width+2+1]
-  ]
+  ];
   const tTetromino =[
     [1,width,width+1,width+2],
     [1,width+1,width+2,width+2+1],
     [width,width+1,width+2,width+2+1],
     [1,width,width+1,width+2+1]
-  ]
+  ];
   const oTetromino = [
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1]
-  ]
+  ];
   const iTetromino = [
     [1,width+1,width+2+1,width+3+1],
     [width,width+1,width+2,width+3],
     [1,width+1,widht+2+1,width+3+1],
     [width,width+1,width+2,width+3]
-  ]
-  const theTetrominoes = [lTetromino,zTetromnino,tTetromino,oTetromino,iTetromino]
-})
+  ];
+  const theTetrominoes = [lTetromino,zTetromnino,tTetromino,oTetromino,iTetromino];
+
+  let currentPosition = 4;
+  let current = theTetrominoes[0][0];
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
+  draw();
+});
