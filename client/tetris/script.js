46c27750c7b3f673eab682f61e32737dc79698d9
Vara <varanikasharma1@gmail.com>
2022-02-14 13:06:42 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -16,36 +16,41 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
   // The Tetromiones
   const lTetromino = [
-    [1, width + 1, (width * 2) + 1, 2],
-    [width, width + 1, width + 2, (width * 2) + 2],
-    [1, width + 1, (width * 2) + 1, width * 2],
-    [width, width * 2, (width * 2) + 1, (width * 2) + 2]
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
   ];
-  const zTetromnino = [
-    [0, width, width + 1, (width * 2) + 1],
-    [width + 1, width + 2, (width * 2), (width * 2) + 1],
-    [0, width, width + 1, (width * 2) + 1],
-    [width + 1, width + 2, width * 2, (width * 2) + 1]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
   ];
+
   const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, (width * 2) + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, (width * 2) + 1]
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
   ];
+
   const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1]
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
   ];
+
   const iTetromino = [
-    [1, width + 1, width * 2 + 1, (width * 3) + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, (width * 3) + 1],
-    [width, width + 1, width + 2, width + 3]
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
   ];
-  const theTetrominoes = [lTetromino, zTetromnino, tTetromino, oTetromino, iTetromino];
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
   let currentPosition = 4;
   let currentRotation = 0;
   let random = Math.floor(Math.random() * theTetrominoes.length);
