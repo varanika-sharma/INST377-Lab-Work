abb4f50029108eec5724f1b2130115ff29252adf
Vara <varanikasharma1@gmail.com>
2022-02-14 12:54:35 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -16,22 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
   // The Tetromiones
   const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2]
+    [1, width + 1, (width * 2) + 1, 2],
+    [width, width + 1, width + 2, (width * 2) + 2],
+    [1, width + 1, (width * 2) + 1, width * 2],
+    [width, width * 2, (width * 2) + 1, (width * 2) + 2]
   ];
   const zTetromnino = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1]
+    [0, width, width + 1, (width * 2) + 1],
+    [width + 1, width + 2, (width * 2), (width * 2) + 1],
+    [0, width, width + 1, (width * 2) + 1],
+    [width + 1, width + 2, width * 2, (width * 2) + 1]
   ];
   const tTetromino = [
     [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
+    [1, width + 1, width + 2, (width * 2) + 1],
     [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1]
+    [1, width, width + 1, (width * 2) + 1]
   ];
   const oTetromino = [
     [0, 1, width, width + 1],
@@ -40,9 +40,9 @@ document.addEventListener('DOMContentLoaded', () => {
     [0, 1, width, width + 1]
   ];
   const iTetromino = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [1, width + 1, width * 2 + 1, (width * 3) + 1],
     [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [1, width + 1, width * 2 + 1, (width * 3) + 1],
     [width, width + 1, width + 2, width + 3]
   ];
   const theTetrominoes = [lTetromino, zTetromnino, tTetromino, oTetromino, iTetromino];
