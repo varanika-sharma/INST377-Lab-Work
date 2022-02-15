83558b34fcddb18729a1d8e44a7406bb4b01ef52
Vara <varanikasharma1@gmail.com>
2022-02-15 12:16:07 -0500

    commit

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -3,26 +3,27 @@ let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel_item');
 const totalSlides = slides.length;
 function updateSlidePosition() {
-  for (const slide of slides) {
+  for (let slide of slides) {
     slide.classList.remove('carousel_item--visible');
     slide.classList.add('carousel_item--hidden');
   }
+  slides[slidePosition].classList.add('carousel_item--visible');
 }
 function moveToNextSlide() {
-  updateSlidePosition();
-  if (slidePosition === totalSlides) {
+  if (slidePosition === totalSlides-1) {
     slidePosition = 0;
   } else {
     slidePosition += 1;
   }
+  updateSlidePosition();
 }
 function moveToPrevSlide() {
-  updateSlidePosition();
   if (slidePosition === 0) {
-    slidePosition = 0;
+    slidePosition = totalSlides-1;
   } else {
     slidePosition -= 1;
   }
+  updateSlidePosition();
 }
 document.getElementById('carousel_button--next').addEventListener('click', () => {
   moveToNextSlide();
