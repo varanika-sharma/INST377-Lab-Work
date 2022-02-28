332a519411ce0d6cc8b9d0b2c068307e9dce13d6
Vara <varanikasharma1@gmail.com>
2022-02-28 10:48:35 -0500

    commit

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   const doodler = document.createElement('div');
   const doodlerLeftSpace = 50;
+  const doodlerBottomSpace = 150;
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add('doodler');
     doodler.style.left = doodlerLeftSpace + 'px';
+    doodler.style.bottom = doodlerBottomSpace + 'px';
   }
   createDoodler();
 })
