e9635c5f3050c33626268f1a3be3aca4eb9726ef
Vara <varanikasharma1@gmail.com>
2022-02-28 13:11:18 -0500

    commit

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -16,6 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let upTimerId;
   let downTimerId;
   let isJumping = true;
+  let isGoingLeft = false;
+  let isGoingRight = false;
+  let leftTimerId;
+  let rightTimerId;
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add('doodler');
@@ -93,21 +97,49 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }, 30);
   }
+  function moveLeft() {
+    if (isGoingRight) {
+      clearInterval(rightTimerId);
+      isGoingRight = false;
+    }
+    isGoingLeft = true;
+    leftTimerId = setInterval(() => {
+      if (doodlerLeftSpace >= 0) {
+        doodlerLeftSpace -= 5;
+        doodler.style.left = doodlerLeftSpace + 'px';
+      } else moveRight();
+    }, 30);
+  }
+  function moveRight() {
+    if (isGoingLeft) {
+      clearInterval(leftTimerId);
+      isGoingLeft = false;
+    }
+    isGoingRight = true;
+    rightTimerId = setInterval(() => {
+      if (doodlerLeftSpace <= 340) {
+        doodlerLeftSpace += 5;
+        doodler.style.left = doodlerLeftSpace + 'px';
+      } else moveLeft();
+    }, 30);
+  }
   function control(e) {
     if (e.key === 'ArrowLeft') {
-      // move left
+      moveLeft();
     } else if (e.key === 'ArrowRight') {
-      // move right
+      moveRight();
     } else if (e.key === 'ArrowUp') {
       // moveStraight
     }
   }
+
   function start() {
     if (!isGameOver) {
       createPlatforms();
       createDoodler();
       setInterval(movePlatforms, 30);
       jump();
+      document.addEventListener('keyup', control);
     }
   }
   start();
