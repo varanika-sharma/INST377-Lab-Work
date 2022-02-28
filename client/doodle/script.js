a0c0d2ccbb8bec4db6bf1e5f0b2d85b85eac0c2c
Vara <varanikasharma1@gmail.com>
2022-02-28 11:10:26 -0500

    commit

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -1,14 +1,43 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable prefer-const */
 /* eslint-disable prefer-template */
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   const doodler = document.createElement('div');
   const doodlerLeftSpace = 50;
   const doodlerBottomSpace = 150;
+  const isGameOver = false;
+  const platformCount = 5;
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add('doodler');
     doodler.style.left = doodlerLeftSpace + 'px';
     doodler.style.bottom = doodlerBottomSpace + 'px';
   }
-  createDoodler();
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom;
+      this.left = Math.random() * 315;
+      this.visual = document.createElement('div');
+      const visual = this.visual;
+      visual.classList.add('platform');
+      visual.style.left = this.left + 'px';
+      visual.style.bottom = this.bottom + 'px';
+      grid.appendChild(visual);
+    }
+  }
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i+=1) {
+      let platGap = 600/platformCount;
+      let newPlatBottom = 100 + i * platGap;
+      let newPlatform = new Platform(newPlatBottom);
+    }
+}
+  function start() {
+    if (!isGameOver) {
+      createDoodler();
+      createPlatforms();
+    }
+  }
+  start();
 })
