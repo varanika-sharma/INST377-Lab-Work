ef77e1adf3abf1016ddba64c691061f44f701eae
Vara <varanikasharma1@gmail.com>
2022-03-15 11:56:55 -0400

    commit

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -51,6 +51,7 @@ async function mainEvent() { // the async keyword means we can make API requests
         item.name.includes(event.target.value);
       });
       // console.log(matchResto);
+    });
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
