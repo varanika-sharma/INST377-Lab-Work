00d53d5edbddef279276153626977aa68574935f
Vara <varanikasharma1@gmail.com>
2022-03-18 13:10:39 -0400

    commit

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -47,10 +47,12 @@ async function mainEvent() { // the async keyword means we can make API requests
       if (currentArray === undefined) { return; }
       console.log(event.target.value);
       const matchResto = currentArray.filter((item) => {
-          console.log(item.name);
-      return item.name.includes(event.target.value);
-      console.log(matchResto);
+        console.log(item);
+        console.log(item.name);
+        return item.name.includes(event.target.value);
       });
+      console.log(matchResto);
+    });
 
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
@@ -66,4 +68,3 @@ async function mainEvent() { // the async keyword means we can make API requests
 }
 // this actually runs first! It's calling the function above
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
-}
