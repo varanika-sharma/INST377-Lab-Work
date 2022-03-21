491415f631f91e43f4c8487052d9a766eae48c16
Vara <varanikasharma1@gmail.com>
2022-03-21 16:17:53 -0400

    commit

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -12,10 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
     bird.style.left = birdLeft + 'px';
   }
   const timerId = setInterval(startGame, 20);
+
   function jump() {
-    birdBottom += 50;
+    if (birdBottom < 500) birdBottom += 50;
     bird.style.bottom = birdBottom + 'px';
     console.log(birdBottom);
   }
+  function control(e) {
+    if (e.keyCode === 32) {
+      jump();
+    }
+  }
+
   document.addEventListener('keyup', jump);
+  function generateObstacle() {
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    gameDisplay.appendChild(obstacle);
+  }
+  generateObstacle();
 });
