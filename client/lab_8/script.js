16504b93454cc10b0ad9656fb1cd7ce874dd1f52
Vara <varanikasharma1@gmail.com>
2022-03-30 11:23:41 -0400

    commit

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -31,7 +31,7 @@ function createHtmlList(collection) {
   });
 }
 function initMap() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const map = L.map('map').setView([38.989697, -76.937759], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
